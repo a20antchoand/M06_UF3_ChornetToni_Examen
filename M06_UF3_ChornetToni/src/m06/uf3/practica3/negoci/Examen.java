dad902aea72ee69d1d0e2ce7a1cc506bce08aa46
a20antchoand <a20antchoand@inspedralbes.cat>
2022-02-02 13:46:05 +0100

    BBDD incrustada

--- M06_UF3_ChornetToni/src/m06/uf3/practica3/negoci/Examen.java
+++ M06_UF3_ChornetToni/src/m06/uf3/practica3/negoci/Examen.java
@@ -1,4 +1,131 @@
 package m06.uf3.practica3.negoci;
 
+import m06.uf3.practica3.presentacio.ExamenInterficie;
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.CreateDB;
+import org.basex.core.cmd.Open;
+import org.basex.core.cmd.XQuery;
+
+import java.io.File;
+import java.util.Scanner;
+
 public class Examen {
+
+    private static Scanner s = new Scanner(System.in);
+    private static ExamenInterficie interficie = new ExamenInterficie();
+    private static Context context;
+    
+    public static void main(String[] args) {
+
+        int opcio = -1;
+        String carrera;
+        //Es mostra el Header
+        interficie.Header();
+
+        do {
+            
+            try {
+
+
+                
+                //Demanem la opcio fins que sigui valida
+                do {
+                    interficie.mostrarMissatge("\nIndica l'opcio a executar: ");
+                    opcio = s.nextInt();
+                } while (opcio < 1 || opcio > 3);
+
+                
+                //Mirem quina funcionalitta hem d'executar
+                if (opcio == 1) {
+
+                    //Es crea la BBDD
+                    crearBBDD();
+
+                    //Mostrem carreres universitaries
+                    mostrarCarreras();
+
+                    //Demanar Carrera al usuari
+                    carrera = interficie.demanaCarrera();
+
+                    //Buscar centre estudiantil
+                    mostrarCentre(carrera);
+                    
+                } else if (opcio == 2) {
+
+                    
+                    
+                }
+                
+            } catch (Exception e) {
+                
+                //Errors
+                e.printStackTrace();
+
+            } finally {
+                
+                //Tancar BBDD
+                try {
+
+                    //Tanquem la BBDD
+                    context.close();
+
+                } catch (Exception e) {
+
+                    //Avisem de que hi ha un error al tancar la BBDD
+                    System.out.println("No se ha podido cerrar la DB");
+
+                }
+            }
+            
+        } while (opcio != 3);
+        
+    }
+
+    private static void mostrarCarreras() throws BaseXException {
+
+        interficie.mostrarMissatge(query("data(for $carrera in universidad//carreras return $carrera//carrera//nombre)\n\n"));
+
+    }
+
+    private static void mostrarCentre(String carrera) throws BaseXException {
+
+        interficie.mostrarMissatge("Facultat: " + query("data(//carrera[nombre='" + carrera + "']/centro)\n\n"));
+
+    }
+
+    private static void crearBBDD() {
+
+        context = new Context();
+
+        try {
+
+            //Intentem obrir la BBDD
+            new Open("universitats").execute(context);
+            interficie.mostrarMissatge("S'ha oberta la BBDD\n\n");
+
+        } catch (Exception e) {
+
+            try {
+
+                //Creem la BBDD
+                interficie.mostrarMissatge("Create a new databae.\n\n");
+                new CreateDB("universitats", "universitats-v2.xml").execute(context);
+                interficie.mostrarMissatge("S'ha creat la BD\n\n");
+
+            } catch (Exception ex) {
+
+                //Avisem de que no s'ha creat la BBDD
+                interficie.mostrarMissatge("No s'ha creat la DB\n\n");
+            }
+
+        }
+
+    }
+
+    //Executem la query i tornem el resultat
+    static public String query(final String query) throws BaseXException {
+        return new XQuery(query).execute(context);
+    }
+
 }
--- M06_UF3_ChornetToni/src/m06/uf3/practica3/presentacio/ExamenInterficie.java
+++ M06_UF3_ChornetToni/src/m06/uf3/practica3/presentacio/ExamenInterficie.java
@@ -1,5 +1,57 @@
 package m06.uf3.practica3.presentacio;
 
+import m06.uf3.practica3_2.negoci.Practica3;
+
+import java.util.Scanner;
+
 public class ExamenInterficie {
+
+    static Scanner s = new Scanner(System.in);
+
+    public void Header () {
+
+        StringBuilder header = new StringBuilder();
+
+        header.append("####################################################################\n");
+        header.append("************PROGRAMA DE CONSULTA DEL FITXER factbook.xml************\n");
+        header.append("***************Versio 1. BD incrustada en el programa***************\n");
+        header.append("####################################################################\n");
+        header.append("\n\n");
+        header.append("## 1. Consulta a Base de dades incrustada.\n");
+        header.append("## 2. Consulta a Base de dades al servidor.\n");
+        header.append("## 3. Sortir.\n");
+        System.out.print(header);
+
+
+    }
+
+    public void Llistat () {
+
+        String llistat = " _-_-_-_-_-_-_-_-_ La llista actual de paisos es: _-_-_-_-_-_-_-_-_" + "\n";
+
+        System.out.println(llistat);
+
+    }
+
+    public String demanaCarrera() {
+        System.out.print("\n\nSiusplau introdueix una carrera: ");
+        return s.nextLine();
+    }
+
+    public void Resultat () {
+        StringBuilder resultat = new StringBuilder();
+        resultat.append("\n\n____________________________________\n");
+        resultat.append("| Les dades del pais escollit son: |\n");
+        resultat.append("____________________________________\n\n");
+
+        System.out.println(resultat);
+
+    }
+
+
+    public void mostrarMissatge(String missatge) {
+        System.out.print(missatge);
+    }
+
 
 }
